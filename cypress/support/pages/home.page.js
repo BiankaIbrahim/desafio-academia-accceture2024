d1e2ce5dbdea9998df94c18dccba06137183fdbd
Bianka Ibrahim <biankaibrahim55@gmail.com>
2024-09-09 14:38:13 -0300

    projeto-testes

--- cypress/support/pages/home.page.js
+++ cypress/support/pages/home.page.js
@@ -1,10 +1,10 @@
 const BTN_ACESSAR_AUTOMOVEL = '#nav_automobile'
 const SECAO_SELECT_INSURE = '#selectedinsurance'
 
-Cypress.Commands.add('acessarSecaoAutomobile', () => {
+Cypress.Commands.add('acessarSecaoAutomovel', () => {
     cy.get(BTN_ACESSAR_AUTOMOVEL).click()
 })
 
-Cypress.Commands.add('validarSecaoAutomolbile', () => {
+Cypress.Commands.add('validarSecaoAutomovel', () => {
     cy.get(SECAO_SELECT_INSURE).should('be.visible')
 })
